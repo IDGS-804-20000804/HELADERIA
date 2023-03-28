3e72a2732483630dc7ef9f8313eae8d511034258
Adrian Hernandez <hernandezsanchezluisadrian1@gmail.com>
2023-03-27 18:50:49 -0600

    Primer commit

--- project/__.init__.py
+++ project/__.init__.py
@@ -1,17 +1,11 @@
-# import flask
-# from flask import Flask, render_template
-# from .models.Student import sql_alchemy
-# from .db.config import DevelopmentConfig
-# from flask_wtf.csrf import CSRFProtect
-# from .routes.students.routes import students as routes_app_students
-# from .routes.teachers.routes import teachers as routes_app_teachers
+import flask
+from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 
-# app = flask.Flask(__name__)
-# app.config['DEBUG'] = True
-# app.config.from_object(DevelopmentConfig)
-# csrf = CSRFProtect()
-# csrf.init_app(app)
-# sql_alchemy.init_app(app)
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 # with app.app_context():
 #     sql_alchemy.create_all()
@@ -19,6 +13,10 @@
 # app.register_blueprint(routes_app_students)
 # app.register_blueprint(routes_app_teachers)
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    csrf.init_app(app)
+    app.run(port=3000)
