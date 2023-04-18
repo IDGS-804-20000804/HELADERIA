7f52f7795fe61bb93bbe370c37891ae5b84b3718
Nestor Andres Peña Bautista <nestorandrespenabautista@gmail.com>
2023-04-18 16:26:57 -0600

    Login Completo

--- project/models/logi/ModeloLogin.py
+++ project/models/logi/ModeloLogin.py
@@ -1,19 +1,20 @@
 from db.db import get_connection 
 from models.entities.User import User
 
-@classmethod
+
 class ModeloLogin():
-    
+    @classmethod
     def inicio(self,db,user):
-        cursor = get_connection()
+        conexion = get_connection()
         try:
-            sql="SELECT id_ususario,email,password FROM usuario WHERE email = ´{}' AND estatus = 1".format(user.email)
-            cursor.execute(sql)
-            row = cursor.fetchone()
-            if row != None:
-                user = User(row[0],row[1], User.check_password(row[2],user.password), row[3])
-                return user 
-            else:
-                return None 
+              with conexion.cursor() as cursor:
+                sql="SELECT id_usuario,correo,contrasenia FROM usuario WHERE correo = '{}' AND estatus = 1".format(user.correo)
+                cursor.execute(sql)
+                row = cursor.fetchone()
+                if row != None:
+                    user = User(row[0],row[1], User.check_password(row[2],user.contrasenia))
+                    return user 
+                else:
+                    return None 
         except Exception as ex:
             raise Exception(ex)
