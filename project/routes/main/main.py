46f09746f58210c997b9d3b4c8ca633895827656
Nestor Andres Peña Bautista <nestorandrespenabautista@gmail.com>
2023-04-20 13:27:56 -0600

    Validacion de Usuarios

--- project/routes/main/main.py
+++ project/routes/main/main.py
@@ -1,3 +1,4 @@
+import ast
 from flask import Blueprint, render_template, request, redirect, url_for,Request,flash
 from flask_wtf.csrf import CSRFProtect
 from db.db import get_connection 
@@ -8,37 +9,40 @@ from flask_security import roles_required, login_required
 main = Blueprint('main', __name__ )
 
 
-@main.route('/main')
 @login_required
+@main.route('/main')
 def mains():
     user_id = current_user.id_usuario
     db = get_connection()
-    print(user_id)
     id_usuario = user_id
     datos = ModeloLogin.get_by_id(db, id_usuario)
-    roles = datos.roles  # asumiendo que roles es un arreglo de números
-    role_labels = []
-    for r in roles:
-        role_labels += get_role_labels(r)
-    return render_template('inicio.html', role_labels=role_labels)
+    roles = datos.roles
+    # role_labels = set()
+    # for r in roles:
+    #     role_labels |= set(get_role_labels(r))
+    # role_labels = list(role_labels)  # convierte de set a list
+    list = ast.literal_eval(datos.roles)
+    return render_template('inicio.html', roles=list)
 
-def get_role_labels(role_num):
-    if role_num == 1:
-        return ['Administrador', 'Comprador', 'Gerente', 'Repartidor', 'Vendedor']
-    elif role_num == 2:
-        return ['Cliente']
-    elif role_num == 3:
-        return ['Vendedor']
-    elif role_num == 4:
-        return ['Repartidor']
-    elif role_num == 5:
-        return ['Comprador']
-    elif role_num == 6:
-        return ['Gerente']
-    elif role_num == 7:
-        return ['Productor']
-    else:
-        return ['']
+# def get_role_labels(role_num):
+#     if role_num == 1:
+#         return ['Administrador', 'Comprador', 'Gerente', 'Repartidor', 'Vendedor']
+#     elif role_num == 2:
+#         return ['Cliente']
+#     elif role_num == 3:
+#         return ['Vendedor']
+#     elif role_num == 4:
+#         return ['Repartidor']
+#     elif role_num == 5:
+#         return ['Comprador']
+#     elif role_num == 6:
+#         return ['Gerente']
+#     elif role_num == 7:
+#         return ['Productor']
+#     else:
+#         return ['']
+
+
 # def mains():
 #     user_id = current_user.id_usuario
 #     db = get_connection()
